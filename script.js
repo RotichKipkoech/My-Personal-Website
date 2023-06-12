fe0c6b40694bd9e261a59f9f92cbf7d3abcd513a
RotichKipkoech <kipkoechrottich@gmail.com>
2023-06-12 09:01:56 +0300

    added form script

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    // Perform form submission handling or AJAX request here
+    alert('Form submitted!');
+  });
