d1177b62698963a089baedf5495f1e7ccbe4cf7f
RotichKipkoech <kipkoechrottich@gmail.com>
2023-06-12 15:49:14 +0300

    updated js

--- script.js
+++ script.js
@@ -65,16 +65,3 @@ function typeWriter(id, ar) {
     }
   }
 }
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    var swiper = new Swiper('.swiper-container', {
-      slidesPerView: 1,
-      spaceBetween: 10,
-      loop: true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-    });
-  });
